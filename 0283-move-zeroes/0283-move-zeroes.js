5c8cc2c2873b76c78391a153628856e4bc0de41c
Hashim <144984297+hashme7@users.noreply.github.com>
2024-06-08 09:03:57 +0530

    Time: 89 ms (17.09%), Space: 53.5 MB (82.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let count = 0 ;
+    for(let i = 0 ;i< nums.length;i++){
+        if(nums[i]==0){
+            nums.splice(i,1);
+            i--;
+            count++;
+        }
+    }
+    while(count){
+        nums.push(0);
+        count--;
+    }
+};
