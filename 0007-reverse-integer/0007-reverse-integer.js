d6c42ee86e2ee9fb09c360ae3759da329a0920a1
Hashim <144984297+hashme7@users.noreply.github.com>
2024-07-03 21:11:08 +0530

    Time: 69 ms (79.12%), Space: 51.8 MB (98.57%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,17 +3,7 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let arr =[];
-    let res = 0;
-    x = x.toString();
-    for(let i =0 ;i< x.length;i++){
-        if(x[i]=='-'){
-            arr.push(x[i]);
-        }else{
-            arr.unshift(x[i]);
-        }
-    }
-    res = parseInt(arr.join(""),10)
+    let res = parseInt(String(x).split('').reverse().join(''),10)
     if(res > Math.pow(2, 31) - 1 || res< -Math.pow(2, 31))return 0;
     return x<0?(res *-1):res;
 };
