2e5b8cc9e1207846a3836385d0bd491a481fc7d8
Hashim <144984297+hashme7@users.noreply.github.com>
2024-10-06 20:46:53 +0530

    Time: 79 ms (91.17%), Space: 62 MB (34.29%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.ts
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.ts
@@ -1,13 +1,9 @@
 function findDisappearedNumbers(nums: number[]): number[] {
-    let hash = {};
-    let result = []
-    for(let i = 0 ;i< nums.length;i++){
-        hash[nums[i]] =1; 
-    }
-    for(let i =1 ;i<=nums.length;i++){
-        if(!hash[i]){
-            result.push(i)
+    let set = new Set(nums), result = [];
+    for(let i = 1;i<=nums.length;i++){
+        if(!set.has(i)){
+            result.push(i);
         }
     }
-    return result;
+    return result
 };
